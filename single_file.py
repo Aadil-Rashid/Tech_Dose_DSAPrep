bc78654fdfb389789d8e339e35bf71787f5edd44
Aadil Rashid Najar <68912638+Aadil-Rashid@users.noreply.github.com>
2022-06-12 00:08:53 +0530

    Update single_file.py

--- single_file.py
+++ single_file.py
@@ -1,6 +1,22 @@
+#python
 class Solution(object):
     def singleNumber(self, nums):
         result = 0
         for i in nums:
             result = result ^ i
         return result 
+
+    #java
+    """
+    class Solution {
+    public int singleNumber(int[] nums) {
+        int i;
+        int result = 0;
+        for (i=0; i< nums.length; i++){
+            result = result ^ nums[i];
+        }
+        return result;
+        
+    }
+}
+    """
